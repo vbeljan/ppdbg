06038c6742fe82ced61638043214af85ed97f763
Vanja Beljan <vanja.beljan@gmx.com>
2019-07-02 16:37:07 +0200

    issue fixes and reader prototype

--- ppdbg_reader.erl
+++ ppdbg_reader.erl
@@ -5,15 +5,13 @@
 -define(PROC, 1).
 -define(TIME, 2).
 -define(CHK, 3).
+-export([main/1]).
 
 main([Path, Sortby]) ->
     dets:open_file(filename:join(Path, ?CHK_EVENT)),
-    Sort = getkey(Sortby),
+    R = dets:foldl(fun(Entry, Acc) -> arrange(Entry, Sortby, Acc) end, [], ?CHK_EVENT),
+    io:format("REPORT: ~n~p", [R]).
 
-    Delimiter = ",".
-    dets:foldl(fun([]) ->
-                       
-
-getkey(proc) -> 1;
-getkey(time) -> 2;
-getkey(checkpoint) -> 3.
+arrange({Proc, Time, Checkpoint}, proc, Acc) -> orddict:append(Proc,{Time, Checkpoint}, Acc);
+arrange({Proc, Time, Checkpoint}, time, Acc) -> orddict:append(Time,{Proc, Checkpoint}, Acc);
+arrange({Proc, Time, Checkpoint}, checkpoint, Acc) -> orddict:append(Checkpoint,{Proc, Time}, Acc).
